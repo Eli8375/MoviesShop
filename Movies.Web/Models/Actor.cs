212471b5823b08b3a4a2c61c4b60ce7d4ccbca84
Eli8375 <86437498+Eli8375@users.noreply.github.com>
2026-03-14 23:29:37 -0700

    added name property to actor model

--- Movies.Web/Models/Actor.cs
+++ Movies.Web/Models/Actor.cs
@@ -1,10 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Movies.Web.Models
 {
     [Table("Actor")]
     public class Actor
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
     }
 }
